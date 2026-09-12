43b59e030632d526e4a7d65f6c69341f23d662aa
santoshnaruje <santoshnaruje@gmail.com>
2026-09-12 14:04:22 +0530

    Solved - Combinations backtracking problem

---
+++ backtracking/combinations.py
@@ -0,0 +1,27 @@
+
+def generateCombinations(n,k):
+        result = []
+        def backtrack(path,index):
+
+            if len(path) == k :
+                result.append(path.copy())
+                return
+
+            if len(path) > k:
+                return
+
+            for num in range(index,n+1):
+                path.append(num)
+                backtrack(path,num+1)
+                path.pop()
+
+
+        backtrack([],1)
+
+        return result
+
+
+
+if __name__ == '__main__':
+
+    print(generateCombinations(4,3))
